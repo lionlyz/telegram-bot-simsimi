a1be19015953bc7948a6d7b14fd6aaba078debab
lionlyz <100843309+lionlyz@users.noreply.github.com>
2022-06-10 20:57:00 +0800

    Update and rename example_config.js to config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
   /**
    * replace the value below with the Telegram token you received from @BotFather
    */
-  TOKEN: "bot_token",
+  TOKEN: "5250118529:AAGjge3COu-7kDQNGnint7yqi6RmuNs7uJg",
 });
